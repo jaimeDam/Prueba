8c02832b4835979a041e7235110de4093dd303b9
DAM1 <DAM1@PC307>
2014-05-26 09:41:14 +0200

    Modificacion clase helicoptero

--- src/com/teide/dam/volador/Helicoptero.java
+++ src/com/teide/dam/volador/Helicoptero.java
@@ -8,7 +8,17 @@ package com.teide.dam.volador;
  *
  * @author DAM1
  */
-public class Helicoptero {
+public class Helicoptero implements Volador{
+
+    @Override
+    public String despegar() {
+       return "El helicoptero esta despegando";
+    }
+
+    @Override
+    public String aterrizar(int metros) {
+       return "El helicoptero ha aterrizado con un coste de " + (metros*0.95) + "€";
+    }
     
     
     
